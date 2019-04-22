f575cc3c064f019b8fc2c649df379b8f90896577
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-04-22 08:48:55 +0100

    quick sketch on the commute

--- import.py
+++ import.py
@@ -1,4 +1,14 @@
 import sys
+import fontforge
+
+font_filename = sys.argv[:-1]
+font = fontforge.open(font_filename)
+
+# select all imported charatcers, thhen apply a "remove overlap" call
+imported_charatcers = []
+font.selection.
+
+font.save(font_filename)
 
 print("Hello World")
 print("The arguments are: " , str(sys.argv))
