af8239702cd30fe5ceab3636143291e97f24a0cd
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-04-25 20:34:49 +0100

    Added validation

--- import.py
+++ import.py
@@ -1,6 +1,17 @@
 import sys
 from fontforge_svg_importer import Font, SvgImporter
 
+
+def print_message(message):
+    print("")
+    print(message)
+    print("")
+
+
+if len(sys.argv) < 3:
+    print_message("Error: Missing input and output filenames")
+    exit(1)
+
 input_sfd_filename = sys.argv[1]
 output_sfd_filename = sys.argv[2]
 svg_filenames = sys.argv[3:]
@@ -16,6 +27,6 @@ for svg_filename in svg_filenames:
 
 font.save_sfd(output_sfd_filename)
 
-print("")
-print("Successfully imported %d glyphs: '%s'" % (glyph_count, output_sfd_filename))
-print("")
+print_message("Successfully imported %d glyphs: '%s'" % (
+    glyph_count, output_sfd_filename
+))
