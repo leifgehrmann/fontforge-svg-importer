59025ea93a66c9a6897e4a6a552a00852f927629
Leif Gehrmann <leif.gehrmann@gmail.com>
2019-04-25 20:17:48 +0100

    Improved log message on success

--- import.py
+++ import.py
@@ -8,8 +8,14 @@ svg_filenames = sys.argv[3:]
 font = Font.open_sfd(input_sfd_filename)
 importer = SvgImporter()
 
+glyph_count = 0
 for svg_filename in svg_filenames:
     glyph = importer.get_glyph_for_svg(font, svg_filename)
     importer.import_outlines_from_svg(glyph, svg_filename)
+    glyph_count += 1
 
 font.save_sfd(output_sfd_filename)
+
+print("")
+print("Successfully imported %d glyphs: '%s'" % (glyph_count, output_sfd_filename))
+print("")
